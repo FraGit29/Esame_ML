1e598e9643b298459f15a8096b4930cad1f90da5
FraGit29 <francesco.furlan29@gmail.com>
2023-05-17 21:53:10 +0200

    21-53

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 import plotly.express as px
 import joblib
 from sklearn.model_selection import train_test_split
