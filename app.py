a92790c100038c2ec0a970ba0ec0407195addc25
FraGit29 <francesco.furlan29@gmail.com>
2023-05-17 22:00:56 +0200

    21-55

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
-import plotly.express as px
+# import plotly.express as px
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
